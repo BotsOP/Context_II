9d55970bf71367c59ebe32cd30e4a85b612a1e4d
Eise <eise.booij@gmail.com>
2023-03-16 09:47:45 +0100

    Finished up resource & shops systems and imported assets

--- Assets/Code/Scripts/Resources & Currency/CurrencyExchanger.cs
+++ Assets/Code/Scripts/Resources & Currency/CurrencyExchanger.cs
@@ -6,6 +6,37 @@ public class CurrencyExchanger : MonoBehaviour
 {
     public GameObject Player;
     public EconomyManager economyManager;
+    private bool InRange;
+
+    private void Start()
+    {
+        InRange = false;
+    }
+
+    private void Update()
+    {
+        if (InRange)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Debug.Log("Exchanging");
+                ExchangeResources();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == 8)
+        {
+            InRange= true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        InRange= false;
+    }
 
     public void ExchangeResources()
     {
@@ -13,9 +44,12 @@ public class CurrencyExchanger : MonoBehaviour
 
         foreach(ResourceScript resource in Player.GetComponent<PlayerInventory>().ListOfRawResources)
         {
+            Debug.Log("Increased total worth");
             totalResourceWorth += resource.worth;
+            Debug.Log("Total resource worth is" + totalResourceWorth);
         }
 
         Player.GetComponent<PlayerInventory>().moneyInInventory += totalResourceWorth;
+        Debug.Log(Player.GetComponent<PlayerInventory>().moneyInInventory);
     }
 }
--- Assets/ShopManager.cs
+++ Assets/ShopManager.cs
@@ -8,8 +8,11 @@ public class ShopManager : MonoBehaviour
     public Dictionary<string, ShopScript> GoodShopDict= new Dictionary<string, ShopScript>();
     public Dictionary<string, ShopScript> BadShopDict = new Dictionary<string, ShopScript>();
 
+    public ShopScript CurrentShop;
+
     private void Start()
     {
+        CurrentShop = null;
         StartCoroutine(waitAndCheckContents());
     }
 
@@ -23,6 +26,26 @@ public class ShopManager : MonoBehaviour
         foreach (KeyValuePair<string, ShopScript> shop in BadShopDict)
         {
             Debug.Log(shop.Key);
+        }
+    }
+
+    public void ClickButton(int button)
+    {
+        switch (button)
+        {
+            case 1:
+                CurrentShop.AddToBasket();
+                break;
+            case 2:
+                CurrentShop.RemoveFromBasket();
+                break;
+            case 3:
+                CurrentShop.ConfirmOrder();
+                break;
+            case 4:
+                CurrentShop.CloseShopWindow();
+                break;
+
         }
     }
 }
